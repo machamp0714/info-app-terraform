e0f052d76391f09c150a184bca156cbaa9c0bc86
25 macamp <rateliasudaisuki@gmail.com>
2019-12-14 16:17:40 +0900

    public subnetを作成

---
+++ vpc/aws_subnet.tf
@@ -0,0 +1,10 @@
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = "10.0.0.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "ap-northeast-1a"
+
+  tags = {
+    Name = "info-public-1a"
+  }
+}
