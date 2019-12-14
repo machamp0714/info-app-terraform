d748f5fe77cd6e3f87c488c14f3d0c230797cbbe
25 macamp <rateliasudaisuki@gmail.com>
2019-12-14 21:34:08 +0900

    ssh通信を許可するsgを作成

---
+++ ec2/aws_sg.tf
@@ -0,0 +1,23 @@
+resource "aws_security_group" "info_app_sg" {
+  name   = "info_app_sg"
+  vpc_id = data.terraform_remote_state.vpc.outputs.vpc_id
+}
+
+resource "aws_security_group_rule" "ingress" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks       = ["123.255.234.116/32"]
+  security_group_id = aws_security_group.info_app_sg.id
+}
+
+resource "aws_security_group_rule" "egress" {
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.info_app_sg.id
+}
+
