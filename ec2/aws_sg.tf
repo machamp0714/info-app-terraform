f405e86a7ed25cfcb93615d51324fbda33cc9c51
25 macamp <rateliasudaisuki@gmail.com>
2019-12-16 10:14:41 +0900

    http通信を許可するsgを追加

--- ec2/aws_sg.tf
+++ ec2/aws_sg.tf
@@ -16,6 +16,16 @@ resource "aws_security_group_rule" "ingress" {
   security_group_id = aws_security_group.info_app_sg.id
 }
 
+resource "aws_security_group_rule" "ingress_http" {
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.info_app_sg.id
+}
+
+
 resource "aws_security_group_rule" "egress" {
   type              = "egress"
   from_port         = 0
